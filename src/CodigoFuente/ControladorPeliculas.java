59e3ecaa25c5b7978dde4e02ca2957e451bb9b36
Alumno <Alumno@ED1-CC2-02>
2022-03-25 12:55:09 -0700

    Se agrego la imprecion

---
+++ src/CodigoFuente/ControladorPeliculas.java
@@ -0,0 +1,16 @@
+package CodigoFuente;
+
+import java.util.ArrayList;
+
+public class ControladorPeliculas {
+
+public static ArrayList<Peliculas> Lista_peliculas =new ArrayList<Peliculas>();
+ 
+public MainControladorPeliculas() {
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/CodigoFuente/ControladorSeries.java
@@ -0,0 +1,17 @@
+package CodigoFuente;
+
+import java.util.ArrayList;
+
+public class ControladorSeries {
+
+public static ArrayList<Series> Lista_seires =new ArrayList<Series>();
+ 
+public MainControladorSeries() {
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+   
